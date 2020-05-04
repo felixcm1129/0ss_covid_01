ab95301dd80a6029171508e77f8129a6165e099c
felixcm1129 <felixcm1129@hotmail.ca>
2020-05-04 17:59:35 -0400

    Terminer 4.1 compléter 4.5 compléter 4.6 compléter

--- Inventaire/Views/MainWindowView.xaml.cs
+++ Inventaire/Views/MainWindowView.xaml.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
 
-namespace Inventaire
+namespace BillingManagement.UI.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
